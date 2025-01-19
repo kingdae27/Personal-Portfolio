d8d9e4f52f5b310b0f04bc839914448e073a4bb5
kingdae27 <165980539+kingdae27@users.noreply.github.com>
2025-01-19 04:04:05 -0500

    feat: add contact form handler script with email functionality

--- form-handler.php
+++ form-handler.php
@@ -1,26 +1,44 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+/**
+ * Contact Form Handler
+ * 
+ * This PHP script processes data submitted via a contact form,
+ * sends an email to the specified recipient, and redirects the user
+ * to a confirmation page.
+ * 
+ * @Daelen Bows
+ * @version 1.1
+ * @date: 01-19-2025
+ */
 
-$email_subject = 'New Form Submission';
+// Collect form data from the POST request
+$name = $_POST['name']; // The name entered by the user
+$visitor_email = $_POST['email']; // The user's email address
+$subject = $_POST['subject']; // The subject of the message
+$message = $_POST['message']; // The content of the message
 
+// Email settings
+$email_from = 'info@yourwebsite.com'; // The sender's email address ()
+$email_subject = 'New Form Submission'; // The subject of the email
+
+// Construct the email body
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                     "Subject: $subject.\n".
-                    "User Message: $message.\n".;
+                    "User Message: $message.\n";
 
+// Recipient email address
 $to = 'daelenjames@icloud.com';
 
+//Headers for the email
 $headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-$headers . = "Reply-To: $visitor_email \r\n";
-
+// Send the email
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location: contact.html") 
+// Redirect to the contat confirmation page
+header("Location: contact.html");
 
 ?>
